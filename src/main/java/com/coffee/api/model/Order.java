1a7870b9863461228981420561f46094a106f564
Lorena Rojas <lochan04@gmail.com>
2023-04-28 15:53:05 -0500

    added table

--- src/main/java/com/coffee/api/model/Order.java
+++ src/main/java/com/coffee/api/model/Order.java
@@ -1,6 +1,22 @@
 package com.coffee.api.model;
 
+import javax.persistence.*;
+import java.time.LocalDate;
 
+@Entity
+// SQL table name
+@Table(name = "orders")
 public class Order {
+    // Creates a table with id, order date, quantity,
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private LocalDate orderDate;
+
+    @Column
+    private int quantity;
 
 }
