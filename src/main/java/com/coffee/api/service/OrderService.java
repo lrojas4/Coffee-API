9ed036144deffb0e239bd9e3d246ecb949259277
Lorena Rojas <lochan04@gmail.com>
2023-04-29 17:46:26 -0500

    added createOrder

--- src/main/java/com/coffee/api/service/OrderService.java
+++ src/main/java/com/coffee/api/service/OrderService.java
@@ -1,14 +1,11 @@
 package com.coffee.api.service;
 
-
 import com.coffee.api.exception.InformationExistException;
 import com.coffee.api.model.Order;
 import com.coffee.api.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
@@ -32,7 +29,7 @@ public class OrderService {
         return orderRepository.findById(orderId).orElse(null);
     }
 
-    // Creates and order if order doesn't already exist
+    // Creates and order if order doesn't already exista
     public Order createOrder(@RequestBody Order orderObject) {
         Order order= orderRepository.findByName(orderObject.getName());
         if (order != null) {
@@ -41,6 +38,4 @@ public class OrderService {
             return orderRepository.save(orderObject);
         }
     }
-
-
 }
