751a60cecd5d7fe8340be4ba28057aeca50206dc
Lorena Rojas <lochan04@gmail.com>
2023-04-29 17:16:20 -0500

    added findByName

--- src/main/java/com/coffee/api/repository/OrderRepository.java
+++ src/main/java/com/coffee/api/repository/OrderRepository.java
@@ -4,7 +4,7 @@ import com.coffee.api.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<Order,Long> {
-    // selects id from order
-    Order findById(String id);
+    Order findByName(String name);
 
+    Order findById(String id);
 }
