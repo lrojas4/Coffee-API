217c69320b514fa11481eeb9e6f890f157271ce0
Lorena Rojas <lochan04@gmail.com>
2023-04-29 17:19:39 -0500

    added getOrders endpoint

--- src/main/java/com/coffee/api/service/OrderService.java
+++ src/main/java/com/coffee/api/service/OrderService.java
@@ -1,8 +1,29 @@
 package com.coffee.api.service;
 
 
+import com.coffee.api.model.Order;
+import com.coffee.api.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Service
 public class OrderService {
+    private OrderRepository orderRepository;
+
+    @Autowired
+    public void setCategoryRepository(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    // Returns a list of all orders
+    @GetMapping(path = "/categories/")
+    public List<Order> getOrders() {
+        return orderRepository.findAll();
+    }
+
+
+
 }
