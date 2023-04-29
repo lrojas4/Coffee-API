3b7804bc0e09950c33564073063596cdc6dc06d3
Lorena Rojas <lochan04@gmail.com>
2023-04-29 14:34:18 -0500

    added InformationNotFoundException

--- src/main/java/com/coffee/api/exception/InformationNotFoundException.java
+++ src/main/java/com/coffee/api/exception/InformationNotFoundException.java
@@ -1,2 +1,12 @@
-package com.coffee.api.exception;public class InformationNotFoundException {
+package com.coffee.api.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class InformationNotFoundException extends RuntimeException {
+    public InformationNotFoundException(String message) {
+        super(message);
+    }
 }
