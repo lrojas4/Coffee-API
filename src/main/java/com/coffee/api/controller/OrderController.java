c78328fa96b28700f132d6ad4d163858dfdba5bf
Lorena Rojas <lochan04@gmail.com>
2023-04-29 13:59:31 -0500

    updated comments

--- src/main/java/com/coffee/api/controller/OrderController.java
+++ src/main/java/com/coffee/api/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.coffee.api.controller;
 
+import com.coffee.api.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 // A combination of @Controller and @ResponseBody annotations.
@@ -10,37 +12,38 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/api")
 
 public class OrderController {
+    private OrderRepository orderRepository;
+
+    @Autowired
+    public void setOrderRepository(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
 
     // Gets all orders
-    // http://localhost:8081/api/orders/
     @GetMapping(path = "/orders/")
     public String getOrders() {
         return "get all orders";
     }
 
     // Gets order by its id
-    // http://localhost:8081/api/orders/1/
     @GetMapping(path = "/orders/{orderId}/")
     public String getOrder(@PathVariable Long orderId) {
         return "creating an order " + orderId;
     }
 
     // Creates an order
-    // http://localhost:8081/api/orders/
     @PostMapping(path = "/orders/")
     public String createOrder(@RequestBody String body) {
         return "creating an order " + body;
     }
 
     // Updates an order by order id
-    // http://localhost:8081/api/orders/1/
     @PutMapping(path = "/orders/{orderId}/")
     public String updateOrder(@PathVariable Long orderId, @RequestBody String body) {
         return "updating the order with order id " + orderId + body;
     }
 
     // Deletes order by order id
-    // http://localhost:8081/api/orders/1/
     @DeleteMapping(path = "/orders/{orderId}/")
     public String deleteOrder(@PathVariable Long orderId) {
         return "deleting the order with the order id " + orderId;
