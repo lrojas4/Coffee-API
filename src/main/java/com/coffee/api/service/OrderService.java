629c60e2077e8d716959a2632900f9015179ab4f
Lorena Rojas <lochan04@gmail.com>
2023-04-29 17:27:21 -0500

    added getOrder

--- src/main/java/com/coffee/api/service/OrderService.java
+++ src/main/java/com/coffee/api/service/OrderService.java
@@ -6,6 +6,7 @@ import com.coffee.api.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -19,11 +20,12 @@ public class OrderService {
     }
 
     // Returns a list of all orders
-    @GetMapping(path = "/categories/")
     public List<Order> getOrders() {
         return orderRepository.findAll();
     }
 
-
-
+    // Returns order by id
+    public Order getOrder(@PathVariable Long orderId) {
+        return orderRepository.findById(orderId).orElse(null);
+    }
 }
