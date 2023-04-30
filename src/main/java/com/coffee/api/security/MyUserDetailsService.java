31f3cf8efd07423286debb6bebd191cd450802f7
Lorena Rojas <lochan04@gmail.com>
2023-04-30 11:13:45 -0500

    added loadUserByUserName

--- src/main/java/com/coffee/api/security/MyUserDetailsService.java
+++ src/main/java/com/coffee/api/security/MyUserDetailsService.java
@@ -1,2 +1,26 @@
-package com.coffee.api.security;public class MyUserDetailsService {
+package com.coffee.api.security;
+
+import com.coffee.api.model.User;
+import com.coffee.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    // loads user by username
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findUserByEmail(email);
+        return new MyUserDetails(user);
+    }
 }
