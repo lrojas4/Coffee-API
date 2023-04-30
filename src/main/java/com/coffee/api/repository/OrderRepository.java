b3dbc479cdec3bd3ed12996d5b2d497b4fb813e2
Lorena Rojas <lochan04@gmail.com>
2023-04-30 13:22:00 -0500

    added findByUserId, findByIdAndUserId, and findByUserIdAndName

--- src/main/java/com/coffee/api/repository/OrderRepository.java
+++ src/main/java/com/coffee/api/repository/OrderRepository.java
@@ -3,8 +3,19 @@ package com.coffee.api.repository;
 import com.coffee.api.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order,Long> {
     Order findByName(String name);
 
     Order findById(String id);
+
+    // get all categories that belong to logged-in user
+    List<Order> findByUserId(Long userId);
+
+    // get category by id and user id
+    Order findByIdAndUserId(Long orderId, Long userId);
+
+    // We are trying to find category with name and user id
+    Order findByUserIdAndName(Long userId, String orderName);
 }
