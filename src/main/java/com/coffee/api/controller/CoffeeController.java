4fc3287970e9a83bcb3640f9f558096cef57be7b
Lorena Rojas <lochan04@gmail.com>
2023-04-29 23:31:59 -0500

    added updateCoffee

--- src/main/java/com/coffee/api/controller/CoffeeController.java
+++ src/main/java/com/coffee/api/controller/CoffeeController.java
@@ -1,7 +1,9 @@
 package com.coffee.api.controller;
 
 import com.coffee.api.model.Coffee;
+import com.coffee.api.repository.OrderRepository;
 import com.coffee.api.service.CoffeeService;
+import com.coffee.api.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,14 +24,29 @@ public class CoffeeController {
         this.coffeeService = coffeeService;
     }
 
+
     // Creates a list of coffee per order id
     @PostMapping(path = "/orders/{orderId}/")
     public Coffee createCoffee(@PathVariable Long orderId, @RequestBody Coffee coffeeObject) {
         return coffeeService.createCoffee(orderId, coffeeObject);
     }
 
+    // Gets coffees from orders
     @GetMapping(path = "/orders/{orderId}/coffees/")
     public List<Coffee> getCoffees(@PathVariable Long orderId) {
         return coffeeService.getCoffees(orderId);
     }
+
+    // Gets coffee from order
+    @GetMapping(path = "/orders/{orderId}/coffees/{coffeeId}/")
+    public Coffee getCoffee(@PathVariable Long orderId, @PathVariable Long coffeeId) {
+        return coffeeService.getCoffee(orderId, coffeeId);
+    }
+
+    // Updates coffee by coffee id from order by order id
+    @PutMapping(path = "/orders/{orderId}/coffees/{coffeeId}")
+    public Coffee updateCoffee(@PathVariable Long orderId, @PathVariable Long coffeeId, @RequestBody Coffee coffeeObject) {
+        return coffeeService.updateCoffee(orderId, coffeeId, coffeeObject);
+    }
+
 }
