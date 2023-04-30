a9a663df9201a38b32566b1c2542ff03c0bf032f
Lorena Rojas <lochan04@gmail.com>
2023-04-29 22:11:57 -0500

    added Coffee Controller

---
+++ src/main/java/com/coffee/api/controller/CoffeeController.java
@@ -0,0 +1,28 @@
+package com.coffee.api.controller;
+
+import com.coffee.api.model.Coffee;
+import com.coffee.api.service.CoffeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+// A combination of @Controller and @ResponseBody annotations.
+// Eliminates the need for annotating each method with @ResponseBody
+@RestController
+
+// Maps the web requests
+@RequestMapping(path = "/api")
+public class CoffeeController {
+
+    private CoffeeService coffeeService;
+
+    @Autowired
+    public void setCoffeeService(CoffeeService coffeeService) {
+        this.coffeeService = coffeeService;
+    }
+
+    // Creates a list of coffee per order id
+    @PostMapping(path = "/orders/{orderId}/")
+    public Coffee createCoffee(@PathVariable Long orderId, @RequestBody Coffee coffeeObject) {
+        return coffeeService.createCoffee(orderId, coffeeObject);
+    }
+}
