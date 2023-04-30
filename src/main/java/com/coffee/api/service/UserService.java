858b37e183a7beed499635fd2df874a710a5e47a
Lorena Rojas <lochan04@gmail.com>
2023-04-30 09:12:19 -0500

    added user repository and password encoder

--- src/main/java/com/coffee/api/service/UserService.java
+++ src/main/java/com/coffee/api/service/UserService.java
@@ -1,9 +1,20 @@
 package com.coffee.api.service;
 
-
+import com.coffee.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 }
