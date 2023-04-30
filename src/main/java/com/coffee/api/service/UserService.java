be2e3910375948b62dcf18b9cf598237fb2a25ac
Lorena Rojas <lochan04@gmail.com>
2023-04-30 11:37:24 -0500

    updated constructor

--- src/main/java/com/coffee/api/service/UserService.java
+++ src/main/java/com/coffee/api/service/UserService.java
@@ -3,8 +3,11 @@ package com.coffee.api.service;
 import com.coffee.api.exception.InformationExistException;
 import com.coffee.api.model.User;
 import com.coffee.api.repository.UserRepository;
+import com.coffee.api.security.JWTUtils;
+import com.coffee.api.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +16,18 @@ public class UserService {
 
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
+    private JWTUtils jwtUtils;
+    private AuthenticationManager authenticationManager;
+    private MyUserDetails myUserDetails;
 
-    @Autowired
-    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder) {
+    // Constructor
+    public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder, JWTUtils jwtUtils,
+                       @Lazy AuthenticationManager authenticationManager, @Lazy MyUserDetails myUserDetails) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+        this.authenticationManager = authenticationManager;
+        this.myUserDetails = myUserDetails;
     }
 
     // creates user
