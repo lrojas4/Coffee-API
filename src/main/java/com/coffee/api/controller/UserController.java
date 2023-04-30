1a11aa0eb611fd0db90359407de1a2ebe0d7d3cd
Lorena Rojas <lochan04@gmail.com>
2023-04-30 09:27:05 -0500

    added user service and createUser

--- src/main/java/com/coffee/api/controller/UserController.java
+++ src/main/java/com/coffee/api/controller/UserController.java
@@ -1,9 +1,29 @@
 package com.coffee.api.controller;
 
+import com.coffee.api.model.User;
+import com.coffee.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/auth/users/")
 public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public void SetUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    // creates User
+    @PostMapping(path = "/register/")
+    public User createUser(@RequestBody User userObject) {
+        return userService.createUser(userObject);
+    }
+
+
 }
