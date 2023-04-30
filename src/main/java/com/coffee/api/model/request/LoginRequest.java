c2a7e82b1dad1f7717171aac6fd9ddb1784195cb
Lorena Rojas <lochan04@gmail.com>
2023-04-30 10:46:49 -0500

    added getters and setters

--- src/main/java/com/coffee/api/model/request/LoginRequest.java
+++ src/main/java/com/coffee/api/model/request/LoginRequest.java
@@ -1,4 +1,29 @@
 package com.coffee.api.model.request;
 
 public class LoginRequest {
+
+    private String email;
+
+    private String password;
+
+
+    // gets email
+    public String getEmail() {
+        return email;
+    }
+
+    // sets email
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // gets password
+    public String getPassword() {
+        return password;
+    }
+
+    // sets password
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
