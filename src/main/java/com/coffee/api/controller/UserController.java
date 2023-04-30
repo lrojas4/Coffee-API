3b163613c5647979175d99f52a5eeb5b999afe31
Lorena Rojas <lochan04@gmail.com>
2023-04-30 08:29:34 -0500

    added RestController and RequestMapping

--- src/main/java/com/coffee/api/controller/UserController.java
+++ src/main/java/com/coffee/api/controller/UserController.java
@@ -1,4 +1,9 @@
 package com.coffee.api.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/auth/users/")
 public class UserController {
 }
