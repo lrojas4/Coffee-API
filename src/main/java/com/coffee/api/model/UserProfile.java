18fe01f58975a69f01dc67ae8d780cc56e821623
Lorena Rojas <lochan04@gmail.com>
2023-04-30 08:49:16 -0500

    created table

--- src/main/java/com/coffee/api/model/UserProfile.java
+++ src/main/java/com/coffee/api/model/UserProfile.java
@@ -1,4 +1,24 @@
 package com.coffee.api.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "profiles")
 public class UserProfile {
+
+    // Creates a table with columns id, firstName, lastName, profileDescription
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String profileDescription;
+
 }
