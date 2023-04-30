4d407d220a89f6f5aa8ed1a827ee530e4d92c668
Lorena Rojas <lochan04@gmail.com>
2023-04-30 09:25:39 -0500

    added createUser and findUserByEmail

--- src/main/java/com/coffee/api/service/UserService.java
+++ src/main/java/com/coffee/api/service/UserService.java
@@ -1,5 +1,7 @@
 package com.coffee.api.service;
 
+import com.coffee.api.exception.InformationExistException;
+import com.coffee.api.model.User;
 import com.coffee.api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -16,5 +18,21 @@ public class UserService {
     public UserService(UserRepository userRepository, @Lazy PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    // creates user
+    public User createUser(User userObject) {
+        if (!userRepository.existsByEmail(userObject.getEmail())) {
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            return userRepository.save(userObject);
+        } else {
+            throw new InformationExistException("user with email address " + userObject.getEmail() +
+                    " already exists");
+        }
+    }
+
+    // finds user by email address
+    public User findUserByEmail(String email) {
+        return userRepository.findUserByEmail(email);
     }
 }
